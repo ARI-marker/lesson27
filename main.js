7523f1f5846a5986e0256ebe277be25e4d1e97ce
ARI-marker <arisheqiri833@gmail.com>
2025-01-29 16:16:33 +0100

    kemi bere function

---
+++ main.js
@@ -0,0 +1,18 @@
+function ShowMessage(){
+    alert("This is message");
+}
+ShowMessage();
+
+function sum(num1, num2) {
+    return num1 + num2;
+}
+
+console.log(sum(5,3));
+
+function toCelcius(f){
+    return (5/9)*(f-32);
+}
+console.log("100 fahreheit is equal to" + toCelcius(100) + "celcius");
+
+
+
